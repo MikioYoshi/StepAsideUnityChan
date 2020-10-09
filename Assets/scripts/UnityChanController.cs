3a8d69f3a895d8485a9d74324e2d1500c5b02b04
MikioYoshi <71675915+MikioYoshi@users.noreply.github.com>
2020-10-09 23:42:05 +0900

    chapter4.8
    chapter4.8　unitychanを左右のキーでコントロールできるようにした。

---
+++ Assets/scripts/UnityChanController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    private Animator myAnimator;
+    private Rigidbody myRigitbody;
+    private float velocityZ = 16f;
+    private float velocityX = 10f;
+    private float movableRange = 3.4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myAnimator = GetComponent<Animator>();
+
+        this.myAnimator.SetFloat ("Speed", 1);
+
+        this.myRigitbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputVelocityX = 0;
+
+        if (Input.GetKey (KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            inputVelocityX = -this.velocityX;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.movableRange > this.transform.position.x)
+        {
+            inputVelocityX = this.velocityX;
+        }
+
+
+        this.myRigitbody.velocity = new Vector3(inputVelocityX, 0, this.velocityZ);
+    }
+}
